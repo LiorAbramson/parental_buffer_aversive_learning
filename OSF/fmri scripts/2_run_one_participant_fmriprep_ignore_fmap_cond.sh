476488856191074285963c533bfcfb5b68c4dc8e
LiorAbramson <54664313+LiorAbramson@users.noreply.github.com>
2023-02-21 07:22:45 -0500

    many changes in fMRI analyses script; added ROI data files

---
+++ OSF/fmri scripts/2_run_one_participant_fmriprep_ignore_fmap_cond.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#SBATCH --account=psych
+#SBATCH --job-name=fmriprep
+#SBATCH -c 12
+#SBATCH --time=11:59:00
+#SBATCH --mem-per-cpu=30gb
+
+# This script runs one participant through fMRIPrep
+# difference from main script is skipping field maps.
+
+# 1) define the participant object name to be input into the fMRIPrep function
+# ID number of subject you want to run; $1 is used when running a batch of participants
+# To run one subject, specify the specific ID number after the --participant flag (e.g. sub-PA001)
+subj=$1
+
+# 2) load the singularity software
+module load singularity
+
+# 3) print which participant fMRIPrep is running on for the purposes of tracking progress / errors
+echo "Running fMRIPrep on participant $subj without fieldmaps"
+
+# 4) Run fmriprep from the singularity container
+singularity exec -e \
+/burg/psych/users/elfk/fmriprep_21.0.1.sif fmriprep \
+/burg/psych/users/elfk/bids/ \
+/burg/psych/users/elfk/la2908/derivatives/01_fmriprep/ \
+--fs-license-file /burg/psych/users/elfk/la2908/code/cond/01_fmriprep/license.txt \
+participant \
+--participant_label $subj \
+--skip-bids-validation \
+--fd-spike-threshold 0.2 \
+--bids-filter-file fmriprep_preproc_cond.json \
+--ignore fieldmaps \
+--nthreads 8 \
+--random-seed 24 \
+--fs-license-file /burg/psych/users/elfk/la2908/code/cond/01_fmriprep/license.txt \
+--fs-no-reconall \
+--output-spaces MNI152NLin6Asym:res-2 \
+--dummy-scans 4
+--skull-strip-t1w force \
+--stop-on-first-crash \
+--notrack \
+--work-dir work \
+
---
+++ OSF/fmri scripts/create_confound_txtfile_lowdof_rm1st4TRs_cond.R
@@ -0,0 +1,74 @@
+
+# This script creates a confound.txt file from the fmriprep *desc-confound_regressors.tsv file
+# It creates regressors separately for each task (cond1, cond2). 
+# It removes the first 4 TRs from the file (dummy TRs, specific to ELFK study)
+
+# Nuisance regressors to include: 
+# a) mean global CSF and mean global WM
+# c) 12 motion regressors (6 motion + their derivatives)
+# d) all censored frames
+
+# load libraries
+library(tidyverse)
+
+# create placeholder for task. Change this line according to whether you want to process cond1 or cond2
+task <- "task-cond2"
+
+# read in data file
+df <- read_tsv(paste0("confound_",task,".tsv"))
+
+#clean dataframe
+df_clean <- df %>%
+  # select columns of interest
+  select(# average WM and CSF signals in derived masks
+         csf,
+         white_matter,
+         # 12 motion parameters
+         trans_x,
+         trans_x_derivative1,
+         trans_y,
+         trans_y_derivative1,
+         trans_z,
+         trans_z_derivative1,
+         rot_x,
+         rot_x_derivative1,
+         rot_y,
+         rot_y_derivative1,
+         rot_z,
+         rot_z_derivative1,
+         # each motion outlier (FD > 0.9 for task-based activation)
+         starts_with("motion_outlier")) %>% 
+  # replace the n/a's with 0's for motion derivatives
+  mutate(trans_x_derivative1 = ifelse(trans_x_derivative1 == "n/a", 0, trans_x_derivative1),
+         trans_y_derivative1 = ifelse(trans_y_derivative1 == "n/a", 0, trans_y_derivative1),
+         trans_z_derivative1 = ifelse(trans_z_derivative1 == "n/a", 0, trans_z_derivative1),
+         rot_x_derivative1 = ifelse(rot_x_derivative1 == "n/a", 0, rot_x_derivative1),
+         rot_y_derivative1 = ifelse(rot_y_derivative1 == "n/a", 0, rot_y_derivative1),
+         rot_z_derivative1 = ifelse(rot_z_derivative1 == "n/a", 0, rot_z_derivative1))
+
+#remove the first 4 dummy TRs
+df_clean_rm1st4TRs <- df_clean[-c(1:4),]
+
+# remove coloumns of bad TR condound regressors that reflect the first 4 dummy TRs    
+which_remove <- c()
+for (i in 15:ncol(df_clean_rm1st4TRs)) {
+  if (sum(df_clean_rm1st4TRs[,i]) == 0){which_remove<-c(which_remove,i)}
+	}
+df_clean_rm1st4TRs <- df_clean_rm1st4TRs[,-which_remove]
+
+
+
+# save cleaned dataframe as a .txt file with tab separated values
+write.table(df_clean, file = paste0("confound_lowdof_",task,".txt"), 
+            sep = "\t", # specify tab separated values
+            col.names = F, # removes names of columns
+            row.names = F, # removes names of rows (in this case, numbers)
+            quote = F) # removes quotes around values in the first row
+
+
+# save cleaned dataframe without the first 4 TRs as a .txt file with tab separated values 
+write.table(df_clean_rm1st4TRs, file = paste0("confound_lowdof_",task,"_rm1st4TRs.txt"),
+            sep = "\t", # specify tab separated values
+            col.names = F, # removes names of columns
+            row.names = F, # removes names of rows (in this case, numbers)
+            quote = F) # removes quotes around values in the first row
